ca463de8b7b7a30bfbb577a2c0ac87db1597ca90
AlanWangsgard <alanwangsgard@byui.edu>
2022-06-10 16:14:48 -0600

    finished

--- met.js
+++ met.js
@@ -9,27 +9,29 @@ async function term() {
         .then(res => res.json()).then(data => data)
     console.log(termdata)
     var section = document.createElement("section")
+    var ul = document.createElement("ul")
     for (let i = 0; i < 20; i++) {
         // termdata.objectIDs.forEach(async function(element) {
         element = termdata.objectIDs[i]
-        var div = document.createElement("div")
-        div.classList.add("card")
+        var li = document.createElement("li")
+        li.classList.add("card")
         data2 = await getData(element)
         console.log(data2)
         var img = document.createElement("img")
         var p = document.createElement("p")
         img.src = data2.primaryImageSmall
         p.innerHTML = data2.title + "<br>" + " Department:" + data2.department
-        div.appendChild(img)
-        div.appendChild(p)
-        section.appendChild(div)
+        li.appendChild(img)
+        li.appendChild(p)
+        ul.appendChild(li)
     }
+    // });
     var terms = document.querySelector("#terms")
     if (terms.innerHTML == "") {
-        terms.appendChild(section)
+        terms.appendChild(ul)
 
     } else {
-        terms.replaceChildren(section)
+        terms.replaceChildren(ul)
     }
     // console.log(terms.innerHTML)
 }
@@ -39,29 +41,29 @@ async function title() {
     var termdata = await fetch("https://collectionapi.metmuseum.org/public/collection/v1/search?q=" + term.value + "title=true")
         .then(res => res.json()).then(data => data)
     console.log(termdata)
-    var section = document.createElement("section")
+    var ul = document.createElement("ul")
     for (let i = 0; i < 20; i++) {
         // termdata.objectIDs.forEach(async function(element) {
         element = termdata.objectIDs[i]
-        var div = document.createElement("div")
-        div.classList.add("card")
+        var li = document.createElement("li")
+        li.classList.add("card")
         data2 = await getData(element)
         console.log(data2)
         var img = document.createElement("img")
         var p = document.createElement("p")
         img.src = data2.primaryImageSmall
         p.innerHTML = data2.title + "<br>" + " Department:" + data2.department
-        div.appendChild(img)
-        div.appendChild(p)
-        section.appendChild(div)
+        li.appendChild(img)
+        li.appendChild(p)
+        ul.appendChild(li)
     }
     // });
     var titles = document.querySelector("#titles")
     if (titles.innerHTML == "") {
-        titles.appendChild(section)
+        titles.appendChild(ul)
 
     } else {
-        titles.replaceChildren(section)
+        titles.replaceChildren(ul)
     }
 }
 
